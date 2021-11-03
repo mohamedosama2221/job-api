96d646a61b074e488b611ba6cef88ae3bea75190
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-11-03 11:22:26 +0400

    'setup

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get("/", (req, res) => {
+  req.send("job api");
+});
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authMiddleware, jobRouter);
